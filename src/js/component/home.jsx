0b0ac4021649e062669c3c03a520a9f61ed68aa6
jamaldonadoa <jadolfomaldonadoa@gmail.com>
2021-10-24 01:10:25 +0000

    listado completo

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,56 @@
-import React from "react";
+import React, { createElement, useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [tareas, setTareas] = useState(
+		document.querySelectorAll("li").length
+	);
+
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<>
+			<div className="container">
+				<div className="card paper">
+					<div className="card-header display-4">
+						<input
+							type="text"
+							name="tarea"
+							id="tarea"
+							placeholder="Introduzca una tarea"
+							onKeyPress={e => {
+								if (
+									e.key === "Enter" &&
+									e.target.value !== ""
+								) {
+									let lista = document.querySelector("ul");
+									let item = document.createElement("li");
+									item.className = "list-group-item";
+									item.innerHTML = `<span><i class="fas fa-minus-circle"></i></span> ${e.target.value}`;
+									item.addEventListener("click", function() {
+										this.remove();
+										setTareas(
+											document.querySelectorAll("li")
+												.length
+										);
+									});
+									lista.appendChild(item);
+									setTareas(
+										document.querySelectorAll("li").length
+									);
+									e.target.value = "";
+								}
+							}}
+						/>
+					</div>
+					<ul className="list-group list-group-flush"></ul>
+					<div className="card-footer">
+						Tareas por hacer: {tareas}
+					</div>
+				</div>
+			</div>
+		</>
 	);
 };
 
